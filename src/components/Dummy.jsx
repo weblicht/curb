64dda69c70dbc5b27c745ced1813666a30c33290
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-07-03 16:00:26 +0200

    src/components/Dummy.jsx: a dummy component, for testing

---
+++ src/components/Dummy.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Dummy extends React.Component {
+
+    render () {
+        return (<div>Hi there! This component was loaded from germanet-common-components.</div>);
+    };
+}
+
+export { Dummy };
