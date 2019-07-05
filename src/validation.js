eb38bb7041e3911e3511085fd2a3966f5ba4dfc2
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-07-05 15:49:57 +0200

    src/validation.js: helpers for user input validation
    A simple ValidationError class for actions and reducers to make use of

---
+++ src/validation.js
@@ -0,0 +1,10 @@
+// validation.js
+// Utilities for dealing with user-input validation
+
+class ValidationError {
+    constructor(msg) {
+        this.message = msg;
+    }
+}
+
+export { ValidationError };
