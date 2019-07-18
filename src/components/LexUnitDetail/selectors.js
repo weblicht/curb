1e6d973eac4f2ac4c27c9eb5c30dac847f2fc901
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-07-18 16:23:34 +0200

    Remove wiktDefs selector from LexUnitDetail/selectors

--- src/components/LexUnitDetail/selectors.js
+++ src/components/LexUnitDetail/selectors.js
@@ -11,18 +11,6 @@ import SI from 'seamless-immutable';
 //                 globalState);
 
  
-export function selectWiktDefs(globalState, props) {
-    try {
-        const selected = globalState.data.wiktDefs.byLexUnitId[props.fetchParams.lexUnitId] || [];
-        return selected;
-    } catch (e) {
-        // if one of the properties in the middle is not defined yet,
-        // it raises a TypeError; this indicates the data is not yet
-        // in the store, and we should return undefined
-        return undefined;
-    }
-
-}
 export function selectExamples(globalState, props) {
     try {
         const selected = globalState.data.lexExamples.byLexUnitId[props.fetchParams.lexUnitId] || [];
