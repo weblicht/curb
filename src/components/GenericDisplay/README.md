053943cfb494b2ef61575065ebb3c469b9aa44e2
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-07-19 16:46:42 +0200

    Abstract generic table-display logic from LexUnit/component

--- src/components/GenericDisplay/README.md
+++ src/components/GenericDisplay/README.md
@@ -5,5 +5,17 @@ are intended to be reused by higher-level components.
 
 ## Components defined here
 
+`DataTable`: given an array of data and formatting information,
+generate an HTML table to display that data
+
+`DataTableRow`: given a data object and a list of fields, generate an
+HTML table row containing its data.
+
+`DataTableHeaders`: given a field map and a list of fields, generate
+an HTML table header
+
 `DefList`: given a list of terms and a co-indexed list of definitions,
 generate an HTML definition list.
+
+`DelimitedArray`: given an array of data, pretty-print this data with
+delimiters as an HTML span.
--- src/components/GenericDisplay/component.jsx
+++ src/components/GenericDisplay/component.jsx
@@ -1,9 +1,68 @@
 // GenericDisplay/component.jsx
 // Definition of generic display components that are reused elsewhere
 
+import { withNullAsString } from '../../helpers';
+
 import React from 'react';
 
-// DefList
+
+// props:
+//   fieldMap :: [ [String, String] ], array mapping data field names to display names
+//   displayFields :: [ String ], array of data fields to be displayed
+//     This should be a subset of the keys in fieldMap.
+export function DataTableHeaders(props) {
+    const fieldMapObj = Object.fromEntries(props.fieldMap);
+    return (
+        <thead>
+          <tr>
+            {props.displayFields.map(
+                field => <th key={field} scope="col">{fieldMapObj[field]}</th>
+            )}
+          </tr>
+        </thead>
+    );
+}
+
+// props:
+//   className :: String
+//   displayFields :: [ String ] 
+//   data :: Object, with at least the following properties:
+//     .id :: an identifier
+//     all the properties listed in displayFields
+export function DataTableRow(props) {
+    return (
+        <tr key={props.data.id} id={props.data.id} className={props.className}>
+              {props.displayFields.map(
+                  (field) => <td>{ withNullAsString(props.data[field]) }</td>
+              )}
+        </tr>
+    );
+}
+
+// props:
+//   className :: String
+//   fieldMap :: [ [String, String] ], array mapping data field names to display names
+//   displayFields :: [ String ], array of data fields to be displayed
+//     This should be a subset of the keys in fieldMap.
+//   data :: [ Object ], the data objects which will be formattted as the rows of the table
+//   displayRowAs (optional) :: Object -> HTML table row,
+//      a component to display a single data object as a table row.
+//      Defaults to DataTableRow.
+export function DataTable(props) {
+    const RowComponent = props.displayRowAs || DataTableRow;
+
+    return (
+        <table className={props.className}>
+          <DataTableHeaders fieldMap={props.fieldMap} displayFields={props.displayFields}/>
+          <tbody>
+            {props.data.map(
+                row => <RowComponent data={row} displayFields={props.displayFields}/>
+            )}
+          </tbody>
+        </table>
+    );
+}
+
 // props:
 //   className :: String  (CSS class for dl)
 //   terms :: [ String ]
@@ -23,3 +82,24 @@ export function DefList(props) {
     );
 }
 
+
+// props:
+//   className :: String
+//   data :: [ Object ]
+//   delimiter (optional) :: String
+//     prepended to all but the first element; defaults to ', '
+//   displayItemAs (optional) :: data object -> String
+//     defaults to withNullAsString helper
+export function DelimitedArray(props) {
+    const delim = props.delimiter || ', ';
+    const formatter = props.displayItemAs || withNullAsString;
+    const formattedItems = props.data.map(
+        (el, idx) => (idx > 0 ? delim : '') + formatter(el)
+    );
+
+    return (
+        <span className={props.className}>
+          {"".concat(...formattedItems)}
+        </span>
+    );
+}
