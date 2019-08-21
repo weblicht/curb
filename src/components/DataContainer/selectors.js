067b86a353712d9399cab2f0a6fc22f3d3681eed
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-08-21 15:43:08 +0200

    DataContainer: delete selectors.js
    This code has been moved into component.jsx for clarity

--- src/components/DataContainer/selectors.js
+++
@@ -1,8 +0,0 @@
-export function selectContainerState(globalState, ownProps) {
-    try {
-        return globalState.dataContainers.byId[ownProps.id];
-    } catch {
-        return undefined;
-    }
-}
-        
