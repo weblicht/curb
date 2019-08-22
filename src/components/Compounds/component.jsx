9cf1fd3e19025c28f8b5d0b2dfe6813dd7a9e2c7
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-08-22 15:14:25 +0200

    Compounds component: better Table and List display components
    using reshaped data

--- src/components/Compounds/component.jsx
+++ src/components/Compounds/component.jsx
@@ -1,11 +1,13 @@
 import { compoundsQueries } from './actions';
 import { selectCompounds } from './selectors';
 import { DataList,
-         DataSelect,
          DataTable,
+         DefList,
+         List,
          ListItem } from '../GenericDisplay/component';
 import { dataContainerFor } from '../DataContainer/component';
 import { connectWithApiQuery } from '../APIWrapper';
+import { withNullAsString } from '../../helpers';
 
 import React from 'react';
 
@@ -19,34 +21,80 @@ const COMPOUND_FIELD_MAP = [
     // head or modifier.
     // the 'root' of the compound, to which modifiers are added:
     ['head', 'Head'], 
-    ['idHead', 'Head Id'],
-    ['propertyHead', 'Head Property'],
-    // up to 2 modifiers, which may each have up to 3 corresponding lexunits:
-    ['mod1', 'Modifier 1'],
-    ['categoryMod1', 'Modifier 1 Category'],
-    ['propertyMod1', 'Modifier 1 Property'],
-    ['idMod1', 'Modifier 1 Id'],
-    ['id2Mod1', 'Modifier 1 Id 2'],
-    ['id3Mod1', 'Modifier 1 Id 3'],
-    ['mod2', 'Modifier 2'],
-    ['categoryMod2', 'Modifier 2 Category'],
-    ['propertyMod2', 'Modifier 2 Property'],
-    ['idMod2', 'Modifier 2 Id'],
-    ['id2Mod2', 'Modifier 2 Id 2'],
-    ['id3Mod2', 'Modifier 2 Id 3'],
+    ['modifier1', 'Modifier 1'], 
+    ['modifier2', 'Modifier 2'], 
 ];
 const COMPOUND_ALL_FIELDS = COMPOUND_FIELD_MAP.map(entry => entry[0]);
 
-// TODO: are these correct?
-const COMPOUND_NOT_SPLIT_VALUES =
-      { 0: 'Undecided',
-        1: 'Do not split',
-        2: 'Split'
-      };
+// Display components for individual compound constituents:
+
+function ConstituentAsDefList(props) {
+    const item = props.data;
+    if (props.isHead)
+        return (<DefList terms={['Lemma', 'Property']} defs={[item.lemma, item.property]}/>);
+    else
+        return (<DefList terms={['Lemma', 'Property', 'Category']} defs={[item.lemma, item.property, item.category]}/>);
+}
 
 // Display components for individual compounds:
 
+function CompoundAsTableRow(props) {
+    const displayFields = props.displayFields || COMPOUND_ALL_FIELDS;
+    return (
+        <tr id={props.data.id} key={props.data.id}>
+          {displayFields.map(
+              function(field) {
+                  switch(field) {
+                  case 'head':
+                      return <td><ConstituentAsDefList isHead={true} data={props.data.head}/></td>;
+                  case 'modifier1':
+                      return <td><ConstituentAsDefList isHead={false} data={props.data.modifier1}/></td>;
+                  case 'modifier2':
+                      return <td><ConstituentAsDefList isHead={false} data={props.data.modifier2}/></td>;
+                  default:
+                      return <td>{withNullAsString( props.data[field] )}</td>;
+                  }
+              }
+          )}
+        </tr>
+    );
+}
+
+// props:
+//   data :: DataObject, the compound 
+function CompoundAsGrid(props) {
+    if (!(props.data.splits)) return null;
+    const fieldMap = [['title', ''],
+                      ['lemma', 'Lemma'],
+                      ['property', 'Property'],
+                      ['category', 'Category']];
+    const displayFields = fieldMap.map(field => field[0]);
+
+    const head = props.data.head.merge({title: 'Head'});
+    const mod1 = props.data.modifier1.merge({title: 'Modifier 1'});
+    const mod2 = props.data.modifier2.merge({title: 'Modifier 2'});
+    const constituents = [head, mod1, mod2];
+    
+    return (
+        <DataTable data={constituents} idFor={obj => obj.id}
+                   fieldMap={fieldMap} displayFields={displayFields}
+                   extras='compound-detail'/>
+    );
+}
+
 // Display components for an array of compound objects:
+
+// props:
+//   data :: [ DataObject ], the compounds 
+function CompoundsAsList(props) {
+    return (
+        <DataList data={props.data} idFor={props.idFor}
+                  choose={props.choose} unchoose={props.unchoose}
+                  select={props.select} unselect={props.unselect}
+                  displayItemAs={props.displayItemAs || CompoundAsGrid}
+                  extras='compounds-container'/>
+    );
+}
 
 // props:
 //   data :: [ DataObject ], the compounds 
@@ -63,9 +111,8 @@ function CompoundsAsTable(props) {
                    select={props.select} unselect={props.unselect}
                    fieldMap={props.fieldMap || COMPOUND_FIELD_MAP}
                    displayFields={props.displayFields || COMPOUND_ALL_FIELDS}
-                   displayItemAs={props.displayItemAs}
-                   extras="compounds-container"
-        />
+                   displayItemAs={props.displayItemAs || CompoundAsTableRow}
+                   extras='compounds-container'/>
     );
 }
  
@@ -75,5 +122,6 @@ var CompoundsContainer = dataContainerFor('Compounds', selectCompounds, compound
 CompoundsContainer = connectWithApiQuery(CompoundsContainer, compoundsQueries.queryActions);
 
 export { CompoundsContainer,
+         CompoundsAsList,
          CompoundsAsTable }; 
 
