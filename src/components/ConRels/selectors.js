c98633125472ff4be0b27650a2a688a66f643bbb
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-09-02 13:03:49 +0200

    ConRels selector: fix hyponyms tree selector
    There were a variety of hard-to-debug problems here that were hidden
    by the catch block in selectors. But I'm pretty sure this is now
    correct.

--- src/components/ConRels/selectors.js
+++ src/components/ConRels/selectors.js
@@ -13,52 +13,63 @@ export function selectConRels(globalState, props) {
  
 export function selectHyponymsTree(globalState, props) {
 
-    function labelFor(conRel) {
-        return conRel.originatingSynsetId + ': ' + conRel.allOrthForms.join(', ');
-    }
- 
-    function nodeFor(conRel) {
-        // we interpret selection of a hyponym *relation* as a user
-        // desire to see the hyponyms of the *related* synset. In this
-        // way, the tree can also be grown further as the result of user
-        // interaction.
+    const rootSynsetId = props.queryParams.synsetId;
+
+    // IDs that the user has selected but we don't yet have data for:
+    var toFetch = [];
+    
+    // for a given synsetId and list of orth forms, return a node in
+    // the tree representing that synset
+    function nodeFor(synsetId, orthForms) {
         var children = [];
-        if (conRel.selected) {
-            children = hyponymsFor(conRel.relatedSynsetId).map(nodeFor);
+        // create nodes for children which have been selected by the user.
+        // Always include children for the root node.
+        if (synsetId === rootSynsetId || props.selectedItemIds.includes(synsetId)) {
+            const hyponyms = hyponymsFor(synsetId);
+            children = hyponyms.map(cr => nodeFor(cr.relatedSynsetId, cr.allOrthForms));
         }
-
-        return {
-            id: conRel.originatingSynsetId,
-            name: labelFor(conRel),
+            
+        return ({
+            id: synsetId,
+            name: synsetId + ': ' + orthForms.join(', '), //TODO
             children
-        }
+        })
+
     }
 
+    // for a given synsetId, returns conrels where this is the
+    // *originating* synset ID; each of these conrels represents a
+    // child node, with ID given by the *related* synset ID
     function hyponymsFor(synsetId) {
         try {
             const conRels = globalState.apiData.conRels.bySynsetId[synsetId] || [];        
             return conRels.filter(cr => cr.conRelType === 'has_hyponym');
+
         } catch (e) {
-            // fetch data for hyponyms that the user has selected, but
-            // we don't yet have in the store:
-            props.query({ synsetId });
+
+            if (!toFetch.includes(synsetId)) {
+                toFetch.push(synsetId);
+            }
+            
             return [];
         }
     }
 
     // construct and return the tree from the root node
     try {
-        const rootSynsetId = props.queryParams.synsetId;
-        const allRels = globalState.apiData.conRels.bySynsetId[rootSynsetId] || [];        
-        const hyponyms = hyponymsFor(rootSynsetId);
 
-        return {
-            id: rootSynsetId,
-            // give the root node a label, even if the root node has
-            // no hyponyms:
-            name: labelFor(allRels[0]),
-            children: hyponyms.map(nodeFor)
-        }
+        // TODO: how can we get the orthforms to generate a name for
+        // the root synset node?  these are not included in the
+        // conrels data
+        var rootNode = nodeFor(rootSynsetId, []);
+
+        // Pass on the list of data to fetch so we can request the
+        // data at a safe point in the component's lifecycle.  (We
+        // can't do it here, because the selector may run several
+        // times before the component actually mounts.)
+        rootNode.toFetch = toFetch;
+
+        return rootNode;
     }
     catch (e) {
         // we don't have any conrel data whatsoever for this synset
