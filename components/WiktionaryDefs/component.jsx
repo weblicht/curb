70345051ea543f3be123a9462f5b3c652c1bb769
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-10-17 16:57:39 +0200

    WiktionaryDefs: make WiktDefsAsList behave like ILIRecordsAsList
    That is: put the word in an <em> and add an ndash.

--- components/WiktionaryDefs/component.jsx
+++ components/WiktionaryDefs/component.jsx
@@ -47,11 +47,10 @@ export const WIKTDEF_ALL_FIELDS = WIKTDEF_FIELD_MAP.map(entry => entry[0]);
 // className and extras props, if given, will be passed on to ListItem
 function WiktDefAsListItem(props) {
     return (
-        // TODO: is this a reasonable default?
         <ListItem id={props.data.id}
                   className={props.className}
                   extras={props.extras}>
-          {props.data.orthForm}: {props.data.wknParaphrase}
+          <em>{props.data.orthForm}</em> &ndash; {props.data.wknParaphrase}
         </ListItem>
     );
 }
