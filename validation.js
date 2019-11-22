59a771212968cb6089b9844032e99086945793ee
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2019-11-22 16:10:52 +0100

    validation.js: rework validation error class
    rename: ValidationError -> ValidationErrors
    add support for both form errors and field errors
    add support for adding more errors after construction time
    convenience method to check if error is empty

--- validation.js
+++ validation.js
@@ -18,10 +18,32 @@
 // validation.js
 // Utilities for dealing with user-input validation
 
-class ValidationError {
-    constructor(msg) {
-        this.message = msg;
+class ValidationErrors {
+    constructor(formErrors = [], fieldErrors = {}) {
+        // formErrors and fieldErrors represent two kinds of
+        // validation errors.
+
+        // formErrors should be a list of strings indicating problems
+        // that are not specific to any one field:
+        this.formErrors = formErrors;
+        
+        // fieldErrors should be an object mapping field names to
+        // errors which are specific to that field:
+        this.fieldErrors = fieldErrors; 
+    }
+
+    addFieldError(fieldName, errorMsg) {
+        this.fieldErrors[fieldName] = errorMsg;
+    }
+
+    addFormError(errorMsg) {
+        this.formErrors.push(errorMsg);
+    }
+
+    isEmpty() {
+        return (this.formErrors.length === 0 &&
+                Object.keys(this.fieldErrors).length === 0)
     }
 }
 
-export { ValidationError };
+export { ValidationErrors };
