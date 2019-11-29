weblicht/curb

commit c9c3c782e9f682553905de07e4254516a43db551

Graphs: accept a forceRedraw prop to unconditionally redraw
This is necessary to allow graphs to display properly whenever they
are not visible on first render. See comment in
VerticalDoubleTreeGraph.componentDidUpdate.

ConRels: pass forceRedraw prop through in tree containers.

components/Graphs/component.jsx
@@ -693,6 +693,12 @@ export class VerticalTreeGraph extends React.Component {
     }
     
     componentDidUpdate(prevProps) {
+        // See related comment in VerticalDoubleTreeGraph: 
+        if (this.props.forceRedraw) {
+            this.drawTree();
+            return;
+        }
+
         if (!isEqual(prevProps.tree, this.props.tree)) {
             this.drawTree();
         }
@@ -791,6 +797,20 @@ export class VerticalDoubleTreeGraph extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
+        // D3VerticalTreeGraph avoids drawing the graph when the SVG
+        // DOM node is not visible.  But there is no way inside React
+        // to notice that this node has *become* visible except via a
+        // change in props or state; thus, we allow components higher
+        // up the hierarchy to send down a forceRedraw prop if the
+        // graph should unconditionally be redrawn. This is useful
+        // when e.g. the graph appears on an unselected tab in a
+        // tabbed interface, and thus it is not visible when the
+        // component first renders.
+        if (this.props.forceRedraw) {
+            this.drawTrees();
+            return;
+        }
+
         // TODO: this still re-draws too often, seemingly on every
         // state change, and not just when the data actually changes.
         // May want to do a deep comparison if performance becomes a

components/ConRels/component.jsx
@@ -145,6 +145,7 @@ function makeNodeClickHandler(props) {
 //   height
 //   nodes
 //   links 
+//   forceRedraw
 function HnymsGraph(props){
     return (
         <VerticalDoubleTreeGraph upwardTree={props.data.children[0]}
@@ -154,6 +155,7 @@ function HnymsGraph(props){
                                  height={props.height}
                                  nodes={props.nodes}
                                  links={props.links}
+                                 forceRedraw={props.forceRedraw}
                                  nodeClickHandler={props.nodeClickHandler || makeNodeClickHandler(props)} />
     );
 }
@@ -167,6 +169,7 @@ function HyponymsGraph(props){
                            height={props.height}
                            nodes={props.nodes}
                            links={props.links}
+                           forceRedraw={props.forceRedraw}
                            nodeClickHandler={props.nodeClickHandler || makeNodeClickHandler(props)}/>
     );
 }
@@ -180,6 +183,7 @@ function HypernymsGraph(props){
                            height={props.height}
                            nodes={props.nodes}
                            links={props.links}
+                           forceRedraw={props.forceRedraw}
                            nodeClickHandler={props.nodeClickHandler || makeNodeClickHandler(props)}/>
 
     );

commit c784f79a202e983e7f5e758681c93132beb19174

Graphs: only redraw on forceRedraw *changes*
This fixes an issue introduced by commit c9c3c78: we want to force a
redraw when the forceRedraw prop is true, but only when it was false
in the immediately-previous props. Otherwise we might redraw too
often, because the Graph component can be rendered multiple times
after the prop flips from false to true. This could have been
contributing to some jerkiness in the graph rendering, though I'm not
sure that's really there.

components/Graphs/component.jsx
@@ -694,7 +694,7 @@ export class VerticalTreeGraph extends React.Component {
     
     componentDidUpdate(prevProps) {
         // See related comment in VerticalDoubleTreeGraph: 
-        if (this.props.forceRedraw) {
+        if (!prevProps.forceRedraw && this.props.forceRedraw) {
             this.drawTree();
             return;
         }
@@ -806,7 +806,7 @@ export class VerticalDoubleTreeGraph extends React.Component {
         // when e.g. the graph appears on an unselected tab in a
         // tabbed interface, and thus it is not visible when the
         // component first renders.
-        if (this.props.forceRedraw) {
+        if (!prevProps.forceRedraw && this.props.forceRedraw) {
             this.drawTrees();
             return;
         }