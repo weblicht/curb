ae7d840ba2444cc7004da04831c71c9a7f2fafbf
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2020-02-14 15:26:22 +0100

    Graphs/selectors.js: selectHnymPaths pull from/to IDs from props
    Since we are now using propsToQueryParams function to generate
    queryParams for APIWrapper, instead of specifying the queryParams
    object directly.

--- components/Graphs/selectors.js
+++ components/Graphs/selectors.js
@@ -1,6 +1,6 @@
 export function selectHnymPaths(globalState, props) {
     try {
-        const pathId = `from${props.queryParams.fromSynsetId}to${props.queryParams.toSynsetId}`;
+        const pathId = `from${props.fromSynsetId}to${props.toSynsetId}`;
         const selected = globalState.apiData.hnymPaths.byId[pathId] || [];
         return selected;
     } catch (e) {
