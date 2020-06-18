7735d1c98ba5613377af882cf5713c9e7e6c3c74
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2020-06-18 12:42:05 +0200

    Graphs: selectHnymPathsState should default to returning an object
    Since this function is passed directly as the mapStateToProps function
    for HnymPathsBetweenGraph, it needs to return an object.  This fixes a
    Redux warning that was appearing in Germanet Rover.

--- components/Graphs/selectors.js
+++ components/Graphs/selectors.js
@@ -1,10 +1,11 @@
 export function selectHnymPathsState(globalState, props) {
+    const dfault = { fetching: undefined, data: undefined, error: undefined };
     try {
         const pathId = `from${props.fromSynsetId}to${props.toSynsetId}`;
-        const selected = globalState.apiData.hnymPaths.byId[pathId] || [];
+        const selected = globalState.apiData.hnymPaths.byId[pathId] || dfault;
         return selected;
     } catch (e) {
-        return { fetching: undefined, data: undefined, error: undefined };
+        return dfault;
     }
 }
 
